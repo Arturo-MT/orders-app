a0dafc2ca0fcea5b85eb9e032f4fab1b6a755042
Arturo Monroy <ingatorrez@gmail.com>
2025-05-12 16:52:57 -0600

    refactor: ajustar la configuración de Metro para utilizar require en lugar de import y eliminar código innecesario

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,8 @@
+/* eslint-disable no-undef */
 // Learn more https://docs.expo.io/guides/customizing-metro
-import { getDefaultConfig } from 'expo/metro-config'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const { getDefaultConfig } = require('expo/metro-config')
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname)
 
-export default config
+module.exports = config
