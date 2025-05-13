e667b4385f37e1523c7c2fcd58b7e1f4fb343be8
Arturo Monroy <ingatorrez@gmail.com>
2025-05-12 21:16:29 -0600

    refactor: agregar función para invalidar consultas de órdenes

--- hooks/api/orders/index.tsx
+++ hooks/api/orders/index.tsx
@@ -1,9 +1,9 @@
 import { useFetch } from '@/app/context/FetchContext'
-import { useMutation, useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { orderQuery, ordersQuery } from './queries'
 import { ORDERS_KEY } from './constants'
-import { ordersMutation } from './mutations'
-import { Order } from '@/types/types'
+import { ordersMutation, orderUpdateMutation } from './mutations'
+import { Order, OrderUpdate } from '@/types/types'
 
 export function useOrdersQuery(config = {}) {
   const { client } = useFetch()
@@ -30,4 +30,22 @@ export function useOrdersMutation(config = {}) {
     mutationFn: (payload: Order) => ordersMutation({ client, payload }),
     ...config
   })
+}
+
+export function useOrderUpdateMutation(id: number, config = {}) {
+  const { client } = useFetch()
+
+  return useMutation({
+    mutationFn: ({ payload }: { payload: OrderUpdate }) =>
+      orderUpdateMutation({ client, payload, id }),
+    ...config
+  })
+}
+
+export function useInvalidateOrders() {
+  const queryClient = useQueryClient()
+
+  return () => {
+    queryClient.invalidateQueries({ queryKey: [ORDERS_KEY] })
+  }
 }
