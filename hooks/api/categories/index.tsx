66fbfab403a137f4b6df875f80c4d13e4a7b487c
Arturo Monroy <ingatorrez@gmail.com>
2026-01-15 17:42:50 -0600

    feat: actualizar useCategoriesQuery y categoriesQuery para integrar soporte de Supabase y manejar la consulta de categorías por tienda

--- hooks/api/categories/index.tsx
+++ hooks/api/categories/index.tsx
@@ -1,13 +1,21 @@
 import { useFetch } from '@/app/context/FetchContext'
+import { useStore } from '@/app/context/StoreContext'
 import { useQuery } from '@tanstack/react-query'
 import { categoriesQuery } from './queries'
 import { CATEGORIES_KEY } from './constants'
 
 export function useCategoriesQuery(config = {}) {
   const { client } = useFetch()
+  const { store, loading: storeLoading } = useStore()
+
   return useQuery({
-    queryKey: [CATEGORIES_KEY],
-    queryFn: () => categoriesQuery({ client }),
+    queryKey: [CATEGORIES_KEY, store?.id],
+    enabled: !!store?.id && !storeLoading,
+    queryFn: () =>
+      categoriesQuery({
+        client,
+        storeId: store!.id
+      }),
     ...config
   })
 }
--- hooks/api/categories/queries.ts
+++ hooks/api/categories/queries.ts
@@ -1,6 +1,24 @@
-import { AxiosInstance } from 'axios'
+import { SupabaseClient } from '@supabase/supabase-js'
 
-export async function categoriesQuery({ client }: { client: AxiosInstance }) {
-  const { data } = await client.get('/product-categories/')
+export async function categoriesQuery({
+  client,
+  storeId
+}: {
+  client: SupabaseClient
+  storeId: string
+}) {
+  const { data, error } = await client
+    .from('product_category')
+    .select('*')
+    .eq('is_active', true)
+    .eq('store_id', storeId)
+    .order('name', { ascending: true })
+
+  console.log('Categories data:', data, 'Store ID:', storeId)
+
+  if (error) {
+    throw error
+  }
+
   return data
 }
