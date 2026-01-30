f0c38edc969d2b97eb96cc9938e466d396fe155f
Arturo Monroy <ingatorrez@gmail.com>
2026-01-30 17:52:25 -0600

    feat: agregar soporte para storeId en la consulta de resumen

--- hooks/api/summary/index.tsx
+++ hooks/api/summary/index.tsx
@@ -1,6 +1,7 @@
 import { useFetch } from '@/app/context/FetchContext'
 import { useQuery } from '@tanstack/react-query'
 import { summaryQuery } from './queries'
+import { useStore } from '@/app/context/StoreContext'
 
 export type SummaryParams = {
   period: 'day' | 'week' | 'month' | 'year'
@@ -11,11 +12,12 @@ const SUMMARY_KEY = 'summary'
 
 export function useSummaryQuery(params: SummaryParams | null) {
   const { client } = useFetch()
+  const { activeStore } = useStore()
 
   return useQuery({
     queryKey: params
-      ? [SUMMARY_KEY, params.period, params.date]
-      : [SUMMARY_KEY],
+      ? [SUMMARY_KEY, params.period, params.date, activeStore?.id]
+      : [SUMMARY_KEY, activeStore?.id],
     queryFn: () => {
       if (!params) {
         throw new Error('No params')
@@ -24,7 +26,8 @@ export function useSummaryQuery(params: SummaryParams | null) {
       return summaryQuery({
         client,
         period: params.period,
-        date: params.date
+        date: params.date,
+        storeId: activeStore!.id
       })
     },
     enabled: !!params
--- hooks/api/summary/queries.ts
+++ hooks/api/summary/queries.ts
@@ -3,16 +3,20 @@ import { SupabaseClient } from '@supabase/supabase-js'
 export async function summaryQuery({
   client,
   period,
-  date
+  date,
+  storeId
 }: {
   client: SupabaseClient
   period: 'day' | 'week' | 'month' | 'year'
   date: string
+  storeId: string
 }) {
-  const { data, error } = await client.rpc('get_summary', {
-    p_period: period,
-    p_date: date
-  })
+  const { data, error } = await client
+    .rpc('get_summary', {
+      p_period: period,
+      p_date: date
+    })
+    .eq('store_id', storeId)
 
   if (error) throw error
   return data
