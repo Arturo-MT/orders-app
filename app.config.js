f28acb4c273d63ca16987caf37510bb28a1c087c
Arturo Monroy <ingatorrez@gmail.com>
2026-01-31 16:47:57 -0600

    feat: mejorar la configuración de variantes para soporte de versiones beta y producción

--- app.config.js
+++ app.config.js
@@ -1,23 +1,31 @@
 export default ({ config }) => {
-  const isDevBuild = process.env.EAS_BUILD_PROFILE === 'development'
+  const variant =
+    process.env.APP_VARIANT || process.env.EAS_BUILD_PROFILE || 'production'
+
+  const isDev = variant === 'development'
+  const isBeta = variant === 'beta'
 
   return {
     ...config,
 
-    name: isDevBuild ? 'Taco Loco (DEV)' : 'Taco Loco',
+    name: isBeta ? 'Taco Loco (BETA)' : isDev ? 'Taco Loco (DEV)' : 'Taco Loco',
 
     android: {
       ...config.android,
-      package: isDevBuild
-        ? 'com.signemammoth.tacoloco.dev'
-        : 'com.signemammoth.tacoloco'
+      package: isBeta
+        ? 'com.signemammoth.tacoloco.beta'
+        : isDev
+          ? 'com.signemammoth.tacoloco.dev'
+          : 'com.signemammoth.tacoloco'
     },
 
     ios: {
       ...config.ios,
-      bundleIdentifier: isDevBuild
-        ? 'com.signemammoth.tacoloco.dev'
-        : 'com.signemammoth.tacoloco'
+      bundleIdentifier: isBeta
+        ? 'com.signemammoth.tacoloco.beta'
+        : isDev
+          ? 'com.signemammoth.tacoloco.dev'
+          : 'com.signemammoth.tacoloco'
     }
   }
 }
